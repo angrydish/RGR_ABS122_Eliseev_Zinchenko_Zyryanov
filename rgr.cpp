6bfa1125f8991d2e2728002878230b434d535409
Your Name <you@example.com>
2022-05-19 16:01:19 +0700

    cout githab

--- rgr.cpp
+++ rgr.cpp
@@ -1,11 +1,10 @@
 ﻿#include <iostream>
 // Шифр Вижинера, Шифровка с помощью квадрата Полибия, Двойная табличная перестановка
-//ihuugyugugiy
+//matvey
 
-
-//sogsidoghudsfghdshsdufigh
+using namespace std;
+//test
 int main()
 {
-    int a = 5, b = 17;
-    std::cout << pow(a,3)+sqrt(b);
+    cout << "test githaba";
 }
