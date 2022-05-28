8d01a1d08a87bb7cd3834721d432019638cdb17f
Никита Елисеев <eliseev.2021@azure.nstu.ru>
2022-05-28 20:48:12 +0700

    теперь проект можно считать многофайловым

--- ecrypt_decrypt_funcs.h
+++ ecrypt_decrypt_funcs.h
@@ -1,1 +1,9 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+string PolybiusSquare_decrypt(string xd, string key);
+string PolybiusSquare_encrypt(string xd, string key);
+string VigenereDecrypt(string text, string key, vector<char>& alphabet);
+string VigenereEncrypt(string text, string key, vector<char>& alphabet);
---
+++ encrypt_decrypt_funcs.cpp
@@ -0,0 +1,328 @@
+#include "ecrypt_decrypt_funcs.h"
+
+string PolybiusSquare_decrypt(string xd, string key)
+{
+    string alph = "abcdefghiklmnopqrstuvwxyz";
+    vector<int> x;
+    vector<int> y;
+    vector<int> x1;
+    vector<int> y1;
+    string crypt;
+    string text;
+
+    //формирование строки (ключ + алфавит) для формирования квадрата
+    alph = key + alph;
+    for (int i = 0; i < xd.size(); i++)
+    {
+        if (xd[i] != ' ')
+        {
+            text.push_back(xd[i]);
+        }
+        else
+        {
+            continue;
+        }
+    }
+    vector<vector<char>> key_square =
+    {
+        {'a','b','c','d','e'},
+        {'f','g','h','i','k'},
+        {'l','m','n','o','p'},
+        {'q','r','s','t','u'},
+        {'v','w','x','y','z'},
+    };
+
+    //формирование правильной строки для квадрата
+    for (int j = 0; j < key.size(); j++)
+    {
+        for (int i = key.size(); i < alph.size(); i++)
+        {
+            if ((alph[i] == alph[j]) || ((alph[i] == alph[j]) && alph[i] == 'i') || (alph[i] == 'i' && alph[j] == 'j'))
+            {
+                for (int k = i; k < alph.size(); k++)
+                {
+                    alph[k] = alph[k + 1];
+                }
+                i = key.size();
+            }
+        }
+    }
+    //формирование квадрата с ключом
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            key_square[i][j] = alph[5 * i + j];
+        }
+    }
+
+    //cout << endl << "Квадрат Полибия с ключом:" << endl;
+    //for (int i = 0; i < 5; i++)
+    //{
+    //    for (int j = 0; j < 5; j++)
+    //    {
+    //        cout << key_square[i][j] << " ";
+    //    }
+    //    cout << endl;
+    //}
+
+
+    //заполнение координат каждой буквы в соответствующие вектора
+    for (int i = 0, ind = 0; i < 5 && ind < text.size(); i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (key_square[i][j] == text[ind])
+            {
+                x.push_back(j);
+                y.push_back(i);
+                i = 0;
+                j = 0;
+                ind += 1;
+            }
+        }
+    }
+
+    //вывод координат символов в квадрате полибия
+    //for (auto i : x)
+    //{
+    //    cout << i << " ";
+    //}
+    //cout << endl;
+    //for (auto i : y)
+    //{
+    //    cout << i << " ";
+    //}
+    //cout << endl << endl;
+    //for (auto i : x1)
+    //{
+    //    cout << i << " ";
+    //}
+    //cout << endl;
+    //for (auto i : y1)
+    //{
+    //    cout << i << " ";
+    //}
+
+
+    for (int i = 0; i < text.size() / 2; i++)
+    {
+        x1.push_back(x[i]);
+        x1.push_back(y[i]);
+    }
+    for (int i = text.size() / 2; i < text.size(); i++)
+    {
+        y1.push_back(x[i]);
+        y1.push_back(y[i]);
+    }
+    string decrypt;
+    for (int i = 0; i < x1.size(); i++)
+    {
+        decrypt.push_back(key_square[y1[i]][x1[i]]);
+    }
+    return decrypt;
+}
+
+string PolybiusSquare_encrypt(string xd, string key)
+{
+    string alph = "abcdefghiklmnopqrstuvwxyz";
+    vector<int> x;
+    vector<int> y;
+    vector<int> x1;
+    vector<int> y1;
+    string crypt;
+    string text;
+
+    //формирование строки (ключ + алфавит) для формирования квадрата
+    alph = key + alph;
+    for (int i = 0; i < xd.size(); i++)
+    {
+        if (xd[i] != ' ')
+        {
+            text.push_back(xd[i]);
+        }
+        else
+        {
+            continue;
+        }
+    }
+    vector<vector<char>> key_square =
+    {
+        {'a','b','c','d','e'},
+        {'f','g','h','i','k'},
+        {'l','m','n','o','p'},
+        {'q','r','s','t','u'},
+        {'v','w','x','y','z'},
+    };
+
+    //формирование правильной строки для квадрата
+    for (int j = 0; j < key.size(); j++)
+    {
+        for (int i = key.size(); i < alph.size(); i++)
+        {
+            if ((alph[i] == alph[j]) || ((alph[i] == alph[j]) && alph[i] == 'i') || (alph[i] == 'i' && alph[j] == 'j'))
+            {
+                for (int k = i; k < alph.size(); k++)
+                {
+                    alph[k] = alph[k + 1];
+                }
+                i = key.size();
+            }
+        }
+    }
+    //формирование квадрата с ключом
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            key_square[i][j] = alph[5 * i + j];
+        }
+    }
+
+    //cout << endl << "Квадрат Полибия с ключом:" << endl;
+    //for (int i = 0; i < 5; i++)
+    //{
+    //    for (int j = 0; j < 5; j++)
+    //    {
+    //        cout << key_square[i][j] << " ";
+    //    }
+    //    cout << endl;
+    //}
+
+
+    //заполнение координат каждой буквы в соответствующие вектора
+    for (int i = 0, ind = 0; i < 5 && ind < text.size(); i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (key_square[i][j] == text[ind])
+            {
+                x.push_back(j);
+                y.push_back(i);
+                i = 0;
+                j = 0;
+                ind += 1;
+            }
+        }
+    }
+
+    //сам процесс шифровки по правилам шифрования с помощью квадрата Полибия
+    for (int i = 0; i < text.size(); i += 2)
+        x1.push_back(x[i]);
+    for (int i = 0; i < text.size(); i += 2)
+        x1.push_back(y[i]);
+
+    for (int i = 1; i < text.size(); i += 2)
+        y1.push_back(x[i]);
+    for (int i = 1; i < text.size(); i += 2)
+        y1.push_back(y[i]);
+
+
+    //вывод координат символов в квадрате полибия
+    //for (auto i : x)
+    //{
+    //    cout << i << " ";
+    //}
+    //cout << endl;
+    //for (auto i : y)
+    //{
+    //    cout << i << " ";
+    //}
+    //cout << endl << endl;
+    //for (auto i : x1)
+    //{
+    //    cout << i << " ";
+    //}
+    //cout << endl;
+    //for (auto i : y1)
+    //{
+    //    cout << i << " ";
+    //}
+
+    //формирование зашифрованного сообщения с помощью квадрата Полибия и соответствующих координат
+    for (int i = 0; i < x1.size(); i++)
+    {
+        crypt.push_back(key_square[y1[i]][x1[i]]);
+    }
+    return crypt;
+}
+
+string VigenereEncrypt(string text, string key, vector<char>& alphabet)
+{
+    vector<int> K1;
+    vector<int> K2;
+    string crypt, crypt1;
+    for (int i = 0; i < text.size(); i++)
+    {
+        for (int j = 0; j < alphabet.size(); j++)
+        {
+            if (text[i] == alphabet[j])
+            {
+                K1.push_back(j);
+            }
+        }
+    }
+    for (int i = 0, j = 0; i < K1.size(); i++)
+    {
+        K2.push_back(key[j] - 97);
+        j++;
+        if (j == key.size())
+        {
+            j = 0;
+        }
+        crypt.push_back(((K1[i] + K2[i]) % 26) + 97);
+    }
+    for (int i = 0, d = 0; i < text.size(); i++)
+    {
+        if (text[i] == ' ')
+        {
+            crypt1.push_back(' ');
+            d++;
+        }
+        else
+        {
+            crypt1.push_back(crypt[i - d]);
+        }
+    }
+    return crypt1;
+}
+
+string VigenereDecrypt(string text, string key, vector<char>& alphabet)
+{
+    vector<int> K1;
+    vector<int> K2;
+    string crypt, crypt1;
+    for (int i = 0; i < text.size(); i++)
+    {
+        for (int j = 0; j < alphabet.size(); j++)
+        {
+            if (text[i] == alphabet[j])
+            {
+                K1.push_back(j);
+            }
+        }
+    }
+    for (int i = 0, j = 0; i < K1.size(); i++)
+    {
+        K2.push_back(key[j] - 97);
+        j++;
+        if (j == key.size())
+        {
+            j = 0;
+        }
+        crypt.push_back((((K1[i] - K2[i]) > 0 ? K1[i] - K2[i] : K1[i] + 26 - K2[i]) % 26) + 97);
+    }
+    for (int i = 0, d = 0; i < text.size(); i++)
+    {
+        if (text[i] == ' ')
+        {
+            crypt1.push_back(' ');
+            d++;
+        }
+        else
+        {
+            crypt1.push_back(crypt[i - d]);
+        }
+    }
+    return crypt1;
+}
