bb57ff18f3e09466c8790f1ff0aab4f3a926d5f3
Матвей Зинченко <mateus.zk@outlook.com>
2022-06-01 02:23:12 +0700

    исправления бага в шифре гронсфельда, когда ключ не правильно дополнялся

--- rgr.cpp
+++ rgr.cpp
@@ -6,28 +6,34 @@
 using namespace std;
 
 // Шифр Гронсфельда, Шифр Плейфера, Простая табличная перестановка
-string Gronsfeld(string text, string key, string encrypted_text, vector<char> alphabet)
+string Gronsfeld(string text, string key, string encrypted_text)
 {
+    setlocale(LC_ALL, "Rus");
     for (int i = 0; key.size() < (count_if(text.begin(), text.end(), [](int c) { return c != ' '; })); i++) {
         key += key[i];
     }
-    for (int i = 0; encrypted_text.size() < (count_if(text.begin(), text.end(), [](int c) { return c != ' '; })); i++) {
-        encrypted_text += char((int)(text[0]) + key[0] - '0');
+    for (int i = 0; i < text.size(); i++) {
+        if (text[i] == ' ') {
+            key.insert(i, 1, char(32));
+        }
+    }
+    for (int i = 0; encrypted_text.size() < text.size(); i++) {
+        if (text[i] == ' ') {
+            encrypted_text.insert(i, 1, char(32));
+        }else encrypted_text += char((int)(text[i]) + key[i] - '0');
     }
     return encrypted_text;
 }
 int main()
 {
-    setlocale(0, "");
+    setlocale(LC_ALL, "Rus");
     string text, key, encrypted_text;
-    int test = 0;
-    vector<char>alphabet;
-    cout << "Введите текст, который хотите зашифровать" << endl;
+    vector<char>alphabet_engl;
+    cout << "Введите сообщение, которое хотите зашифровать: ";
     getline(cin, text);
-    cout << "Введите ключ шифровки" << endl;
+    cout << "Введите ключ шифровки: ";
     getline(cin, key);
-    for (int i = 65; i < 91; i++) alphabet.push_back(char(i));
-    cout << Gronsfeld(text, key, encrypted_text, alphabet) << endl;
-    test = (int)(text[0]) + key[0] - '0'; // вывод значения аски
-    cout << test;
+    for (int i = 65; i < 91; i++) alphabet_engl.push_back(char(i));
+    encrypted_text = Gronsfeld(text, key, encrypted_text);
+    cout << "Зашифрованное сообщение: " << encrypted_text << endl;
 }
