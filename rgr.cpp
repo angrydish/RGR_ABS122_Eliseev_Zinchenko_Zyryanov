d4c7c80f04469de49af26a66b8e942e9e6ae508a
Матвей Зинченко <mateus.zk@outlook.com>
2022-06-01 03:59:13 +0700

    добавлена проверка входного сообщение на принадлежность языка(русский или английский). теперь в функции гронсфельда зашифрованное сообщение находится с помощью алфавита, а не с помощью таблицы аски

--- rgr.cpp
+++ rgr.cpp
@@ -2,13 +2,13 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include "windows.h"
 
 using namespace std;
 
 // Шифр Гронсфельда, Шифр Плейфера, Простая табличная перестановка
-string Gronsfeld(string text, string key, string encrypted_text)
+string Gronsfeld(string text, string key, string encrypted_text, vector<char>&alphabet)
 {
-    setlocale(LC_ALL, "Rus");
     for (int i = 0; key.size() < (count_if(text.begin(), text.end(), [](int c) { return c != ' '; })); i++) {
         key += key[i];
     }
@@ -20,20 +20,33 @@ string Gronsfeld(string text, string key, string encrypted_text)
     for (int i = 0; encrypted_text.size() < text.size(); i++) {
         if (text[i] == ' ') {
             encrypted_text.insert(i, 1, char(32));
-        }else encrypted_text += char((int)(text[i]) + key[i] - '0');
+        }
+        else {
+            auto n = find(alphabet.begin(), alphabet.end(), text[i]); 
+            int index = n - alphabet.begin();
+            encrypted_text += alphabet[index + key[i]-'0'];
+        }
     }
     return encrypted_text;
 }
 int main()
 {
     setlocale(LC_ALL, "Rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     string text, key, encrypted_text;
     vector<char>alphabet_engl;
+    for (int i = 97; i < 123; i++) alphabet_engl.push_back(char(i));
+    vector<char>alphabet_rus;
+    for (int i = 224; i < 256; i++) alphabet_rus.push_back(char(i));
     cout << "Введите сообщение, которое хотите зашифровать: ";
     getline(cin, text);
     cout << "Введите ключ шифровки: ";
     getline(cin, key);
-    for (int i = 65; i < 91; i++) alphabet_engl.push_back(char(i));
-    encrypted_text = Gronsfeld(text, key, encrypted_text);
+    if (find(alphabet_engl.begin(), alphabet_engl.end(), text[0]) != alphabet_engl.end()) {
+        encrypted_text = Gronsfeld(text, key, encrypted_text, alphabet_engl);
+    }else {
+        encrypted_text = Gronsfeld(text, key, encrypted_text, alphabet_rus);
+    }
     cout << "Зашифрованное сообщение: " << encrypted_text << endl;
 }
