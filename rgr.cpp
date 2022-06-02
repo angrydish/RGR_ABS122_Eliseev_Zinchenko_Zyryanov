6aea69089b9bd184913dd2445aaace15bb3a317c
Матвей Зинченко <mateus.zk@outlook.com>
2022-06-02 15:49:58 +0700

    была реализована функция дешифровки шифра Гронсфельда

--- rgr.cpp
+++ rgr.cpp
@@ -7,8 +7,9 @@
 using namespace std;
 
 // Шифр Гронсфельда, Шифр Плейфера, Простая табличная перестановка
-string Gronsfeld(string text, string key, string encrypted_text, vector<char>&alphabet)
+string Gronsfeld_Encrypt(string text, string key, vector<char>&alphabet)
 {
+    string encrypted_text;
     for (int i = 0; key.size() < (count_if(text.begin(), text.end(), [](int c) { return c != ' '; })); i++) {
         key += key[i];
     }
@@ -34,12 +35,39 @@ string Gronsfeld(string text, string key, string encrypted_text, vector<char>&al
     }
     return encrypted_text;
 }
+string Gronsfeld_Decrypt(string text, string key, vector<char>& alphabet)
+{
+    string decrypt_text;
+    for (int i = 0; key.size() < (count_if(text.begin(), text.end(), [](int c) { return c != ' '; })); i++) {
+        key += key[i];
+    }
+    for (int i = 0; i < text.size(); i++) {
+        if (text[i] == ' ') {
+            key.insert(i, 1, char(32));
+        }
+    }
+    for (int i = 0; decrypt_text.size() < text.size(); i++) {
+        if (text[i] == ' ') {
+            decrypt_text.insert(i, 1, char(32));
+        }
+        else {
+            auto n = find(alphabet.begin(), alphabet.end(), text[i]);
+            int index = n - alphabet.begin() - (key[i] - '0');
+            if (index < 0) {
+                decrypt_text += alphabet[(index + size(alphabet))];
+            }else {
+                decrypt_text += alphabet[index];
+            }
+        }
+    }
+    return decrypt_text;
+}
 int main()
 {
     setlocale(LC_ALL, "Rus");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    string text, key, encrypted_text;
+    string text, key, slovo;
     vector<char>alphabet_engl;
     for (int i = 97; i < 123; i++) alphabet_engl.push_back(char(i));
     vector<char>alphabet_rus;
@@ -49,9 +77,14 @@ int main()
     cout << "Введите ключ шифровки: ";
     getline(cin, key);
     if (find(alphabet_engl.begin(), alphabet_engl.end(), text[0]) != alphabet_engl.end()) {
-        encrypted_text = Gronsfeld(text, key, encrypted_text, alphabet_engl);
+        slovo = Gronsfeld_Encrypt(text, key, alphabet_engl);
+        cout << "Зашифрованное сообщение: " << slovo << endl;
+        slovo = Gronsfeld_Decrypt(slovo, key, alphabet_engl);
+        cout << "Расшифрованное сообщение: " << slovo << endl;
     }else {
-        encrypted_text = Gronsfeld(text, key, encrypted_text, alphabet_rus);
+        slovo = Gronsfeld_Encrypt(text, key, alphabet_rus);
+        cout << "Зашифрованное сообщение: " << slovo << endl;
+        slovo = Gronsfeld_Decrypt(slovo, key, alphabet_rus);
+        cout << "Расшифрованое сообщение: " << slovo << endl;
     }
-    cout << "Зашифрованное сообщение: " << encrypted_text << endl;
 }
